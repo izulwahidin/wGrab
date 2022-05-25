45f242a6a57526b34e7712a4e505f3b197e64101
Izul Wahidin <izul@wahid.in>
2022-05-25 09:44:05 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('class.php');
-$page = new gogoplay5('https://gogoplay5.com/');
-$pageData = $page->minify()->pages();
-print_r($pageData);
-
-$stream = new gogoplay5('https://gogoplay5.com/videos/dna-ova-episode-3');
-$pageStream = $stream->minify()->stream();
-print_r($pageStream);
